5662841b6262b73eae2dcbe172b1b5eb10e721c1
Sakshi Khairnar <88443513+SakshiKhairnar@users.noreply.github.com>
2021-08-10 13:07:46 +0530

    Update index.py

--- index.py
+++ index.py
@@ -10,8 +10,8 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 #minMaxLoc() is used to find brightest pixel in the image
 (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
 
-#Draw the circle to the brightest pixel with specified radius , color & width .
+#Draw the circle to the brightest pixel with specified radius , color & width
 cv2.circle(image, maxLoc,67, (0,0, 255), 10)
 
-# display the results of our newly improved method.
+# display the results of our newly improved method
 im=cv2.resize(image, (960,800))
