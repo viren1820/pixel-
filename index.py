caf14052dbd0f3d5030692dd9e6da71e4d0c6b1d
Soham Mohite <78905925+sohammohite@users.noreply.github.com>
2021-08-10 13:04:18 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,11 @@
 import numpy as np
 import cv2
 
-# load the image and convert it to grayscale.
+# load the image and convert it to grayscale
 image = cv2.imread("input.jpg")
+
+#convert the image into gray color
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+#minMaxLoc() is used to find brightest pixel in the image
+(minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
